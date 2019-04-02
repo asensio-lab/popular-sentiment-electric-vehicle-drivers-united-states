6a039701b4c567fa63d4c8623a84d0534b881145
sha70 <35703291+sha70@users.noreply.github.com>
2019-04-02 11:40:36 -0400

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 12:42:22 2019
+
+@author: Susie
+"""
+
+
+lr = 1e-3 #learning rate
+num_filters = 100
+dropout_percent = 0.6
+filter_sizes = [3,4,5] 
+l2_constraint = None # None or range of XX
+num_epoch = 3
+batch_size = 128
+test_size=0.2
+
+#replication of the paper
+# tuning to beat the 84.14%, prec. 87%, rec. 83%
+# sample size training vs accuracy
+# filter sizes vs accuracy
+
+
+
+# print out all the parameters after each run
+# a separate script for visualizing 
+
+
